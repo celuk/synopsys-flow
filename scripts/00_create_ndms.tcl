c136dab53ef69e61346247161e520002a88e9375
celuk <seyyid4091@gmail.com>
2024-11-04 04:30:04 +0300

    update create ndms script

--- scripts/00_create_ndms.tcl
+++ scripts/00_create_ndms.tcl
@@ -19,7 +19,7 @@ set ANT_SCM_FILE ""
 set_app_options -name file.gds.trace_terminal_type -value PG
 set_app_options -list {file.gds.port_type_map {{power VDDPST} {power VDD} {ground VSS}}}
 
-create_workspace stdcell -technology $TECH_FILE -flow normal
+create_workspace stdcell -technology $TECH_FILE -flow exploration
 
 read_parasitic_tech -layermap $PARASITICS_MAP_FILE -name maxTLU -tlup $PARASITICS_MAX_TLUPLUS_FILE
 read_parasitic_tech -layermap $PARASITICS_MAP_FILE -name nomTLU -tlup $PARASITICS_NOM_TLUPLUS_FILE
@@ -32,27 +32,11 @@ read_db ${STDCELL_DB_PREFIX}wc.db
 read_lef $STDCELL_LEF_FILE
 
 read_gds $STDCELL_GDS_FILE -layer_map $GDSOUT_MAP_FILE
+
+group_libs
 
 check_workspace
 commit_workspace -output ${NDM_PATH}/stdcell.ndm -force
-remove_workspace
-
-create_workspace stdcell_physical_only -technology $TECH_FILE -flow physical_only
-
-read_parasitic_tech -layermap $PARASITICS_MAP_FILE -name maxTLU -tlup $PARASITICS_MAX_TLUPLUS_FILE
-read_parasitic_tech -layermap $PARASITICS_MAP_FILE -name nomTLU -tlup $PARASITICS_NOM_TLUPLUS_FILE
-read_parasitic_tech -layermap $PARASITICS_MAP_FILE -name minTLU -tlup $PARASITICS_MIN_TLUPLUS_FILE
-
-read_db ${STDCELL_DB_PREFIX}tc.db
-read_db ${STDCELL_DB_PREFIX}bc.db
-read_db ${STDCELL_DB_PREFIX}wc.db
-
-read_lef $STDCELL_LEF_FILE
-
-read_gds $STDCELL_GDS_FILE -layer_map $GDSOUT_MAP_FILE
-
-check_workspace
-commit_workspace -output ${NDM_PATH}/stdcell_physical_only.ndm -force
 remove_workspace
 
 exit
