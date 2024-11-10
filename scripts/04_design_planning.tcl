d180c4386fe18aea104b759f9e56192aac03e0e2
celuk <seyyid4091@gmail.com>
2024-11-10 04:47:43 +0300

    remove macro pns

--- scripts/04_design_planning.tcl
+++ scripts/04_design_planning.tcl
@@ -47,13 +47,6 @@ remove_routes -net_types {power ground} -ring -stripe -macro_pin_connect -lib_ce
 ### create pg regions for all macros
 remove_pg_regions -all
 set macros_col [get_cells -physical_context -filter "is_hard_macro==true" -quiet]
-set memory_top $macros_col
-set region_cnt 0
-foreach_in_col _macro $memory_top {
-    set macro_bbox [get_att ${_macro} bbox] ;# check the difference between boundary bbox and bbxo
-    create_pg_region -polygon $macro_bbox MEMORY_REGION_TOP_${region_cnt}
-    incr region_cnt
-}
 
 create_pg_std_cell_conn_pattern pattern_pg_rail -layers M1 -rail_width {@w} -parameters {w}
 create_pg_wire_pattern pattern_stripe -layer @l -direction @d -width @w -spacing @s -pitch @p -track_alignment @t -parameters {l d w s p t} 
@@ -73,11 +66,6 @@ create_pg_composite_pattern pattern_core_m8_mesh_risc -nets {VDDPST VSS} -add_pa
 
 create_pg_composite_pattern pattern_core_m9_mesh -nets {VDD VDDPST VSS} -add_patterns {{{pattern: pattern_wire_based_on_track} {nets: {VDD VDDPST VSS}} {parameters: {M9 horizontal 0.64  0.32  3.20 }}{offset: 0.1 }}} 
 
-set memory_regions_top [get_pg_regions MEMORY_REGION_TOP_* -quiet]
-set_pg_strategy strategy_m6_pg_mesh_top -pattern {{name: pattern_core_m6_mesh_top} {nets: {VDD VSS}}} -blockage {pg_regions: $memory_regions_top} -voltage_areas DEFAULT_VA
-set_pg_strategy strategy_m7_pg_mesh_top -pattern {{name: pattern_core_m7_mesh_top} {nets: {VDD VSS}}} -blockage {pg_regions: $memory_regions_top} -voltage_areas DEFAULT_VA
-set_pg_strategy strategy_m8_pg_mesh_top -pattern {{name: pattern_core_m8_mesh_top} {nets: {VDD VSS}}} -blockage {pg_regions: $memory_regions_top} -voltage_areas DEFAULT_VA
-
 set_pg_strategy_via_rule via_pg_core -via_rule { \
 {{{strategies: strategy_m9_pg_mesh}{layers: M9}}{{strategies: strategy_m8_pg_mesh_top}{layers: M8}}{via_master:default} } \
 {{{strategies: strategy_m9_pg_mesh}{layers: M9}}{{strategies: strategy_m8_pg_mesh_risc}{layers: M8}}{via_master:default} } \
@@ -90,20 +78,6 @@ set_pg_strategy_via_rule via_pg_core -via_rule { \
 {{intersection: adjacent}{via_master: default}} }
 
 compile_pg -strategies {strategy_m6_pg_mesh_top strategy_m6_pg_mesh_risc strategy_m7_pg_mesh_top strategy_m7_pg_mesh_risc strategy_m8_pg_mesh_top strategy_m8_pg_mesh_risc strategy_m9_pg_mesh} -tag pg_stripes -via_rule {via_pg_core} -ignore_via_drc
-
-### create macro ring and pin connection
-create_pg_ring_pattern pattern_memory_ring -horizontal_layer M5 -horizontal_width {1} -vertical_layer M6 -vertical_width {1} -corner_bridge false
-set_pg_strategy strategy_memory_ring_top  -macros $memory_top -pattern { {pattern: pattern_memory_ring} {nets: {VSS VDD}}  {offset: {0.3 0.3}} }
-set_pg_strategy_via_rule strategy_memory_ring_vias -via_rule { \
-    {{{strategies: {strategy_memory_ring_top}} {layers: {M5}}} {existing: {strap }}{via_master: {default}}} \
-    {{{strategies: {strategy_memory_ring_top}} {layers: {M6}}} {existing: {strap }}{via_master: {default}}} \
-}
-compile_pg -strategies {strategy_memory_ring_top} -via_rule {strategy_memory_ring_vias}
-
-### connect macro pins
-create_pg_macro_conn_pattern pattern_memory_pin -pin_conn_type scattered_pin -layers {M5 M6}
-set_pg_strategy strategy_top_pins -macros $memory_top -pattern { {pattern: pattern_memory_pin} {nets: {VSS VDD}} }
-compile_pg -strategies {strategy_top_pins strategy_risc_pins}
 
 check_pg_connectivity
 check_pg_drc
