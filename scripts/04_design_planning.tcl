02bef29e01c93a02ddbf919208d6234f0698ae81
celuk <seyyid4091@gmail.com>
2024-12-06 11:34:42 +0300

    fix pg drcs

--- scripts/04_design_planning.tcl
+++ scripts/04_design_planning.tcl
@@ -54,29 +54,17 @@ create_pg_ring_pattern pg_ring -horizontal_layer M9 \
                                -vertical_layer M8 \
                                -vertical_width {4} \
                                -vertical_spacing {2}
-
-create_pg_ring_pattern pg_ring2 -horizontal_layer M8 \
-                               -horizontal_width {4} \
-                               -horizontal_spacing {2} \
-                               -vertical_layer M9 \
-                               -vertical_width {4} \
-                               -vertical_spacing {2}
-
 # -corner_bridge true
-set_pg_strategy s_core_ring -core -pattern {{pattern: pg_ring}{nets: {VDD VSS}}{offset: {2 2}}} \
-                            -extension {{stop: core_boundary}}
-
-set_pg_strategy s_core_ring2 -core -pattern {{pattern: pg_ring2}{nets: {VSS VDD}}{offset: {20 20}}} \
+set_pg_strategy s_core_ring -core -pattern {{pattern: pg_ring}{nets: {VDD VSS VDD VSS VDD VSS VDD VSS}}{offset: {2 2 2 2 2 2 2 2}}} \
                             -extension {{stop: core_boundary}}
 compile_pg -strategies s_core_ring
-compile_pg -strategies s_core_ring2
 
 create_pg_mesh_pattern pg_mesh -layers {{{vertical_layer: M8} {spacing: 10} \
-                                         {width: 5} {pitch: 100} {trim: false}} \
+                                         {width: 5} {pitch: 90} {trim: false}} \
                                         {{horizontal_layer: M9} {spacing: 10} \
                                          {width: 5} {pitch: 100} {trim: false}}}
 set_pg_strategy s_mesh -pattern {{pattern: pg_mesh} {nets: {VDD VSS}} {offset_start: 100 100}} \
-                       -core -extension {{stop: outermost_ring}}
+                       -core -extension {{stop: innermost_ring}}
 compile_pg -strategies s_mesh
 
 create_pg_std_cell_conn_pattern pg_std_cell_rail -layers {M1}
@@ -85,12 +73,36 @@ compile_pg -strategies s_std_cell_rail
 
 set iopads [get_cells -physical_context -filter "design_type==pad" -quiet]
 create_pg_macro_conn_pattern macro_connect_pattern_vss \
--pin_conn_type scattered_pin -nets {VDD VSS} \
--width {5 5} -layers {M9 M8}
+-pin_conn_type scattered_pin -nets {VSS} \
+-width {4 4} -layers {M2 M2}
 set_pg_strategy s_macro_connect_vss \
--pattern {{name: macro_connect_pattern_vss} {nets: VDD VSS}} \
+-pattern {{name: macro_connect_pattern_vss} {nets: VSS}} \
 -macros "$iopads"
 compile_pg -strategies s_macro_connect_vss
+
+create_pg_macro_conn_pattern macro_connect_pattern_vss1 \
+-pin_conn_type scattered_pin -nets {VDD} \
+-width {4 4} -layers {M9 M8}
+set_pg_strategy s_macro_connect_vss1 \
+-pattern {{name: macro_connect_pattern_vss1} {nets: VDD}} \
+-macros "$iopads"
+compile_pg -strategies s_macro_connect_vss1
+
+#set_app_options -name plan.pgroute.hmpin_connection_target_layers -value M8
+#
+#create_pg_macro_conn_pattern io_to_ring -pin_conn_type scattered_pin \
+#    -pin_layers {M9} -layers {M9 M8} -nets {VDD VSS} -width 4 \
+#    -via_rule {{{intersection: all} {via_master: NIL}}}
+#
+#set_pg_strategy s_io_to_ring -macros $iopads \
+#    -pattern {{name: io_to_ring}{nets: VDD VSS}}
+#
+#set_pg_strategy_via_rule rule1 -via_rule { \
+#   {{{strategies: s_io_to_ring}{layers: M9}} {{existing: ring}{layers: M8}} \
+#    {via_master: default}} {{intersection: undefined} {via_master: NIL}}}
+#
+#compile_pg -strategies s_io_to_ring -via_rule rule1
+
 
 connect_pg_net -automatic
 connect_pg_net -net VDD [get_pins -hierarchical */VDD]
