cd8ddf282abce472b08523110cb94ed6e2927733
celuk <seyyid4091@gmail.com>
2024-12-13 17:06:41 +0300

    comment unnecessary corners

--- data/sdc/c0_soc.sdc
+++ data/sdc/c0_soc.sdc
@@ -1,23 +1,22 @@
 create_clock -name clk_i -period 10 [get_ports {clk_i}]
+set_load  0.012 [all_outputs]
 
 #set_dont_touch_network      [all_clocks]
 #set_fix_hold                [all_clocks]
-set_clock_uncertainty  0.1  [all_clocks]
-set_clock_latency      1.0  [all_clocks]
+#set_clock_uncertainty  0.1  [all_clocks]
+#set_clock_latency      1.0  [all_clocks]
 
-set_input_transition 0.2 [all_inputs]
+#set_input_transition 0.2 [all_inputs]
 
-set_input_delay  -max 1.0 -clock $CLK [remove_from_collection [all_inputs] [get_ports {$CLK}]]
-set_input_delay  -min 0.0 -clock $CLK [remove_from_collection [all_inputs] [get_ports {$CLK}]]
-set_output_delay -max 1.0 -clock $CLK [all_outputs]
-set_output_delay -min 0.0 -clock $CLK [all_outputs]
+#set_input_delay  -max 1.0 -clock $CLK [remove_from_collection [all_inputs] [get_ports {$CLK}]]
+#set_input_delay  -min 0.0 -clock $CLK [remove_from_collection [all_inputs] [get_ports {$CLK}]]
+#set_output_delay -max 1.0 -clock $CLK [all_outputs]
+#set_output_delay -min 0.0 -clock $CLK [all_outputs]
 
 #set_input_delay -clock [get_clocks clk_i] -max 1 [all_inputs] 
 #set_input_delay -clock [get_clocks clk_i] -min 0 [all_inputs]
 #set_output_delay -clock [get_clocks clk_i] -max 1 [all_outputs]
 #set_output_delay -clock [get_clocks clk_i] -min 0 [all_outputs]
-
-set_load  0.012 [all_outputs]
 
 #set_false_path -to [get_pins c0_top/user_processor_dut/isl_blksiz/buyruk_onbellegi_denetleyici_dut/iomem_addr_reg/D]
 #set_false_path -to [get_pins c0_top/user_processor_dut/isl_blksiz/buyruk_onbellegi_denetleyici_dut/l1b_bekle_o_reg/D]
--- scripts/mcmm.tcl
+++ scripts/mcmm.tcl
@@ -53,43 +53,43 @@ set_timing_derate -late 1.04
 set_timing_derate -early 0.96
 set_scenario_status FUNC_132_BC -all -active true -leakage_power true -dynamic_power true
 
-create_corner LT
-set_parasitics_parameters -early_spec minTLU -late_spec minTLU -corners {LT}
-create_mode FUNC_13240
-current_mode FUNC_13240
-create_scenario -mode FUNC_13240 -corner LT -name FUNC_13240_LT
-current_scenario FUNC_13240_LT
-read_sdc $CONSTRAINT_FILE
-current_corner LT
-current_mode FUNC_13240
-current_scenario FUNC_13240_LT
-set_process_number 1.00
-set_temperature -40
-set_voltage -object_list [get_cells -filter {design_type == pad}] 2.75
-set_voltage -object_list VDD 1.32
-set_voltage -object_list VSS 0.0
-set_timing_derate -late 1.04
-set_timing_derate -early 0.96
-set_scenario_status FUNC_13240_LT -all -active true -leakage_power true -dynamic_power true
+#create_corner LT
+#set_parasitics_parameters -early_spec minTLU -late_spec minTLU -corners {LT}
+#create_mode FUNC_13240
+#current_mode FUNC_13240
+#create_scenario -mode FUNC_13240 -corner LT -name FUNC_13240_LT
+#current_scenario FUNC_13240_LT
+#read_sdc $CONSTRAINT_FILE
+#current_corner LT
+#current_mode FUNC_13240
+#current_scenario FUNC_13240_LT
+#set_process_number 1.00
+#set_temperature -40
+#set_voltage -object_list [get_cells -filter {design_type == pad}] 2.75
+#set_voltage -object_list VDD 1.32
+#set_voltage -object_list VSS 0.0
+#set_timing_derate -late 1.04
+#set_timing_derate -early 0.96
+#set_scenario_status FUNC_13240_LT -all -active true -leakage_power true -dynamic_power true
 
-create_corner ML
-set_parasitics_parameters -early_spec minTLU -late_spec minTLU -corners {ML}
-create_mode FUNC_132125
-current_mode FUNC_132125
-create_scenario -mode FUNC_132125 -corner ML -name FUNC_132125_ML
-current_scenario FUNC_132125_ML
-read_sdc $CONSTRAINT_FILE
-current_corner ML
-current_mode FUNC_132125
-current_scenario FUNC_132125_ML
-set_process_number 1.00
-set_temperature 125
-set_voltage -object_list [get_cells -filter {design_type == pad}] 2.75
-set_voltage -object_list VDD 1.32
-set_voltage -object_list VSS 0.0
-set_timing_derate -late 1.04
-set_timing_derate -early 0.96
-set_scenario_status FUNC_132125_ML -all -active true -leakage_power true -dynamic_power true
+#create_corner ML
+#set_parasitics_parameters -early_spec minTLU -late_spec minTLU -corners {ML}
+#create_mode FUNC_132125
+#current_mode FUNC_132125
+#create_scenario -mode FUNC_132125 -corner ML -name FUNC_132125_ML
+#current_scenario FUNC_132125_ML
+#read_sdc $CONSTRAINT_FILE
+#current_corner ML
+#current_mode FUNC_132125
+#current_scenario FUNC_132125_ML
+#set_process_number 1.00
+#set_temperature 125
+#set_voltage -object_list [get_cells -filter {design_type == pad}] 2.75
+#set_voltage -object_list VDD 1.32
+#set_voltage -object_list VSS 0.0
+#set_timing_derate -late 1.04
+#set_timing_derate -early 0.96
+#set_scenario_status FUNC_132125_ML -all -active true -leakage_power true -dynamic_power true
 
 create_corner WC
 set_parasitics_parameters -early_spec maxTLU -late_spec maxTLU -corners {WC}
@@ -110,40 +110,40 @@ set_timing_derate -late 1.04
 set_timing_derate -early 0.96
 set_scenario_status FUNC_108_WC -all -active true -leakage_power true -dynamic_power true
 
-create_corner WCL
-set_parasitics_parameters -early_spec maxTLU -late_spec maxTLU -corners {WCL}
-create_mode FUNC_10840
-current_mode FUNC_10840
-create_scenario -mode FUNC_10840 -corner WCL -name FUNC_10840_WCL
-current_scenario FUNC_10840_WCL
-read_sdc $CONSTRAINT_FILE
-current_corner WCL
-current_mode FUNC_10840
-current_scenario FUNC_10840_WCL
-set_process_number 1.00
-set_temperature -40
-set_voltage -object_list [get_cells -filter {design_type == pad}] 2.25
-set_voltage -object_list VDD 1.08
-set_voltage -object_list VSS 0.0
-set_timing_derate -late 1.04
-set_timing_derate -early 0.96
-set_scenario_status FUNC_10840_WCL -all -active true -leakage_power true -dynamic_power true
+#create_corner WCL
+#set_parasitics_parameters -early_spec maxTLU -late_spec maxTLU -corners {WCL}
+#create_mode FUNC_10840
+#current_mode FUNC_10840
+#create_scenario -mode FUNC_10840 -corner WCL -name FUNC_10840_WCL
+#current_scenario FUNC_10840_WCL
+#read_sdc $CONSTRAINT_FILE
+#current_corner WCL
+#current_mode FUNC_10840
+#current_scenario FUNC_10840_WCL
+#set_process_number 1.00
+#set_temperature -40
+#set_voltage -object_list [get_cells -filter {design_type == pad}] 2.25
+#set_voltage -object_list VDD 1.08
+#set_voltage -object_list VSS 0.0
+#set_timing_derate -late 1.04
+#set_timing_derate -early 0.96
+#set_scenario_status FUNC_10840_WCL -all -active true -leakage_power true -dynamic_power true
 
-create_corner WCZ
-set_parasitics_parameters -early_spec maxTLU -late_spec maxTLU -corners {WCZ}
-create_mode FUNC_1080
-current_mode FUNC_1080
-create_scenario -mode FUNC_1080 -corner WCZ -name FUNC_1080_WCZ
-current_scenario FUNC_1080_WCZ
-read_sdc $CONSTRAINT_FILE
-current_corner WCZ
-current_mode FUNC_1080
-current_scenario FUNC_1080_WCZ
-set_process_number 1.00
-set_temperature 0
-set_voltage -object_list [get_cells -filter {design_type == pad}] 2.25
-set_voltage -object_list VDD 1.08
-set_voltage -object_list VSS 0.0
-set_timing_derate -late 1.04
-set_timing_derate -early 0.96
-set_scenario_status FUNC_1080_WCZ -all -active true -leakage_power true -dynamic_power true
+#create_corner WCZ
+#set_parasitics_parameters -early_spec maxTLU -late_spec maxTLU -corners {WCZ}
+#create_mode FUNC_1080
+#current_mode FUNC_1080
+#create_scenario -mode FUNC_1080 -corner WCZ -name FUNC_1080_WCZ
+#current_scenario FUNC_1080_WCZ
+#read_sdc $CONSTRAINT_FILE
+#current_corner WCZ
+#current_mode FUNC_1080
+#current_scenario FUNC_1080_WCZ
+#set_process_number 1.00
+#set_temperature 0
+#set_voltage -object_list [get_cells -filter {design_type == pad}] 2.25
+#set_voltage -object_list VDD 1.08
+#set_voltage -object_list VSS 0.0
+#set_timing_derate -late 1.04
+#set_timing_derate -early 0.96
+#set_scenario_status FUNC_1080_WCZ -all -active true -leakage_power true -dynamic_power true
